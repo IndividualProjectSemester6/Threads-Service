89288f2fc445460fe37c8f05ff8b8ae8412a13bc
Calin Peters <calinpeters98@hotmail.nl>
2022-11-24 13:17:29 +0100

    Clean up controller

--- src/ThreadsService.API/Controllers/ThreadsController.cs
+++ src/ThreadsService.API/Controllers/ThreadsController.cs
@@ -1,61 +1,59 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using ThreadsService.API.Models;
-using ThreadsService.Application.Commands;
 using ThreadsService.Application.Commands.CreateThread;
 using ThreadsService.Application.Queries.GetAllThreads;
 using ThreadsService.Application.Queries.GetThread;
 using ThreadsService.Domain.Entities;
 
-namespace ThreadsService.API.Controllers
+namespace ThreadsService.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ThreadsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ThreadsController : ControllerBase
+    private readonly IMediator _mediator;
+
+    public ThreadsController(IMediator mediator)
     {
-        private readonly IMediator _mediator;
+        _mediator = mediator;
+    }
 
-        public ThreadsController(IMediator mediator)
+    [HttpGet]
+    public async Task<ActionResult> GetAll()
+    {
+        var query = new GetAllThreadsQuery();
+        var result = await _mediator.Send(query);
+        List<ThreadViewModel> threads = new List<ThreadViewModel>();
+        foreach (var thread in result)
         {
-            _mediator = mediator;
+            threads.Add(new ThreadViewModel(thread.Id, thread.TopicName, thread.Content, thread.Author, thread.CreatedAt, thread.LastEdited));
         }
 
-        [HttpGet]
-        public async Task<ActionResult> GetAll()
-        {
-            var query = new GetAllThreadsQuery();
-            var result = await _mediator.Send(query);
-            List<ThreadViewModel> threads = new List<ThreadViewModel>();
-            foreach (var thread in result)
-            {
-                threads.Add(new ThreadViewModel(thread.Id, thread.TopicName, thread.Content, thread.Author, thread.CreatedAt, thread.LastEdited));
-            }
+        return Ok(threads);    
+    }
 
-            return Ok(threads);    
+    [HttpGet("{threadId}")]
+    public async Task<ActionResult> Get(Guid threadId)
+    {
+        var query = new GetThreadQuery(threadId);
+        var result = await _mediator.Send(query);
+        if (result != null)
+        {
+            var thread = new ThreadViewModel(result.Id, result.TopicName, result.Content, result.Author, result.CreatedAt, result.LastEdited);
+            return Ok(thread);
         }
 
-        [HttpGet("{threadId}")]
-        public async Task<ActionResult> Get(Guid threadId)
-        {
-            var query = new GetThreadQuery(threadId);
-            var result = await _mediator.Send(query);
-            if (result != null)
-            {
-                var thread = new ThreadViewModel(result.Id, result.TopicName, result.Content, result.Author, result.CreatedAt, result.LastEdited);
-                return Ok(thread);
-            }
+        return NotFound();
+    }
 
-            return NotFound();
-        }
+    [HttpPost]
+    public async Task<ActionResult> Create(CreateThreadViewModel createThreadViewModel)
+    {
+        var query = new CreateThreadCommand(new ThreadDto() {Author = createThreadViewModel.Author, TopicName = createThreadViewModel.TopicName, Content = createThreadViewModel.Content});
+        var result = await _mediator.Send(query);
+        if (result == null) return BadRequest();
 
-        [HttpPost]
-        public async Task<ActionResult> Create(CreateThreadViewModel createThreadViewModel)
-        {
-            var query = new CreateThreadCommand(new ThreadDto() {Author = createThreadViewModel.Author, TopicName = createThreadViewModel.TopicName, Content = createThreadViewModel.Content});
-            var result = await _mediator.Send(query);
-            if (result == null) return BadRequest();
-
-            return Ok(result);
-        }
+        return Ok(result);
     }
 }
